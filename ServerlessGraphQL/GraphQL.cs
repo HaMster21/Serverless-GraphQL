9c853fa46fd92d93cace9348351eb029d8a57f26
Hans Meyer <hans.meyer@team-beverage.de>
2019-11-17 17:40:49 +0100

    Equip the endpoint with graphql services
    In order to process requests in graphql, we need a query executor. Function level dependency injection, to my knowledge, is not yet working in Azure Functions, so we'll use constructor injection instead.
    
    We're not yet parsing requests in graphql, this will be the next step.

--- ServerlessGraphQL/GraphQL.cs
+++ ServerlessGraphQL/GraphQL.cs
@@ -1,19 +1,23 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
+using HotChocolate.Execution;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace ServerlessGraphQL
 {
-    public static class GraphQL
+    public class GraphQL
     {
+        private readonly IQueryExecutor Executor;
+
+        public GraphQL(IQueryExecutor executor)
+            => Executor = executor ?? throw new System.ArgumentNullException(nameof(executor));
+
+
         [FunctionName("graphql")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
