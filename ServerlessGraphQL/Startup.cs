2a542e77495839ed27524fef0e64c4ae595f2e6d
Hans Meyer <hans.meyer@team-beverage.de>
2019-11-17 17:22:11 +0100

    Create Startup class
    In order to use dependency injection and configure HotChocolate, we use a functions startup class. It works like a regular startup class you'd write for asp.net and asp.net core, though it's autogenerated if not declared explicitly.
    
    HotChocolate uses the IoC capabilities of asp.net core to configure and use services. They are injected into the resolvers so you can use them

---
+++ ServerlessGraphQL/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(ServerlessGraphQL.Startup))]
+
+namespace ServerlessGraphQL
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
